70f3a25e53e7af898d88a7e01f7182d5b4efd622
Daniel Goncalves <danielgoncalvesti@gmail.com>
2024-08-24 10:01:44 -0300

    corrigido exercício 10

--- Aula03/01-estrutura-sequencial/Ex10.py
+++ Aula03/01-estrutura-sequencial/Ex10.py
@@ -17,8 +17,8 @@ num2 = int(input("Digite o segundo número: "))
 num3 = int(input("Digite o terceiro número: "))
 
 todos_iguais = (num1 == num2) and (num2 == num3) and (num1 == num3)
-todos_diferentes =
-algum_maior_que_50 =
+todos_diferentes = (num1 != num2) and (num2 != num3) and (num1 != num3)
+algum_maior_que_50 = (num1 > 50) or (num2 > 50) or (num3 > 50)
 
 # Exibe os resultados
 print(f"Todos os números são iguais? {todos_iguais}")
