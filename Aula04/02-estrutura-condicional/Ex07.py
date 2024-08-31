37481d1f3170ba0e7abe8fef979dbe3eaa7c89da
Daniel <danielgoncalvesti@gmail.com>
2024-08-30 22:33:46 -0300

    adiciona enunciado do ex 07

--- Aula04/02-estrutura-condicional/Ex07.py
+++ Aula04/02-estrutura-condicional/Ex07.py
@@ -1,3 +1,19 @@
+"""
+Exercício 07 - Escreva um programa que recebe o valor total
+e aplica descontos em compras:
+-5% para compras até R$100,
+-10% para compras acima de R$100 até R$500, e
+-15% para compras acima de R$500.
+Exiba o valor original, o valor do desconto e o valor final.
+
+Entrada:
+Digite o valor total das suas compras (em R$): 200
+
+Saída:
+Valor total: R$ 200.00
+Valor com desconto: R$ 180.00
+Desconto: R$ 20.00
+"""
 def calcular_desconto(valor_total):
     # implemente a função
     TODO
