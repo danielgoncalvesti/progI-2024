1b5351ba8186a74583bf618b5755c6be7c21415a
Daniel <danielgoncalvesti@gmail.com>
2024-08-30 22:37:03 -0300

    atualiza

---
+++ Aula04/02-estrutura-condicional/Ex08.py
@@ -0,0 +1,9 @@
+"""
+Exercício 08 – Crie um programa que solicita ao usuário um número de 1 a 7 e
+determina o dia da semana correspondente (utilizando elif).
+Por exemplo, 1 representa domingo, 2 representa segunda-feira e assim por diante.
+Entrada:
+Digite um número de 1 a 7: 4
+Saída:
+Quarta-feira
+"""
