0121cd00811691668e802c49f7e467a006930704
Sander <87549789+RikelmySander@users.noreply.github.com>
2024-10-09 14:17:31 -0300

    Update Ex06.py

--- Aula02/01-estrutura-sequencial/Ex06.py
+++ Aula02/01-estrutura-sequencial/Ex06.py
@@ -8,5 +8,5 @@ Exemplo de saída:
 A palavra em minúsculas é: python
 
 """
-Palavra = input("Digite uma palavra: ")
-print(Palavra.lower())
+palavra = input("Digite uma palavra: ")
+print(palavra.lower())
