0967484b7cd83c67ff02c150204e4ba2f653b598
Sander <87549789+RikelmySander@users.noreply.github.com>
2024-10-09 14:22:26 -0300

    Update Ex11.py

--- Aula02/01-estrutura-sequencial/Ex11.py
+++ Aula02/01-estrutura-sequencial/Ex11.py
@@ -11,9 +11,9 @@ Ambos são pares? False
 Ambos são ímpares? False
 Um é par e o outro ímpar? True
 """
-Num1 = int(input("Digite um numero: "))
-Num2 = int(input("Digite um numero: "))
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite um numero: "))
 
-print(f"Ambos são pares: {Num1%2==0 and Num2%2 == 0}")
-print(f"Ambos são impares: {Num1%2 == 1 and Num2%2 == 1}")
-print(f"Um par e o outro é impar: {Num1%2==0 and Num2%2 == 1 or Num1%2 == 1 and Num2%2 == 0}")
+print(f"Ambos são pares: {num1%2==0 and num2%2 == 0}")
+print(f"Ambos são impares: {num1%2 == 1 and num2%2 == 1}")
+print(f"Um par e o outro é impar: {num1%2==0 and num2%2 == 1 or num1%2 == 1 and num2%2 == 0}")
