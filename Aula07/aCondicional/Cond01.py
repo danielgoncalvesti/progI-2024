93df523b0202285798322f1200c6a8075c264fea
Sander <87549789+RikelmySander@users.noreply.github.com>
2024-10-09 14:30:15 -0300

    Update Cond01.py

--- Aula07/aCondicional/Cond01.py
+++ Aula07/aCondicional/Cond01.py
@@ -1,7 +1,7 @@
 """
 Escreva um programa que pede um número inteiro e imprime se ele é par ou ímpar.
 """
-NUM1 = input("Digite um numero inteiro: ")
+num = input("Digite um numero inteiro: ")
 
 if num%2 ==0:
     print("Esse numero é par!")
