3f098e0993fc09dea91b20c5290f878ea34f3a97
RikelmySander <rikelmysander@gmail.com>
2024-10-19 07:40:37 -0300

    Arquivo da aula 7 pasta de repetiçoes while Ex.2 resolvido.

--- Aula07/cRepeticao/aWhile/While02.py
+++ Aula07/cRepeticao/aWhile/While02.py
@@ -4,7 +4,7 @@ Após isso, exiba a tabuada do número digitado.
 Implemente a função imprime_tabuada que recebe um número inteiro
 e imprime a tabuada desse número.
 """
-def imprime_tabuada(numero: str):
+def imprime_tabuada(numero: int):
     multiplicador = 1
     while multiplicador < 11:
         print(f"{numero} X {multiplicador} = {numero*multiplicador}")
